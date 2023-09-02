5509f15634a4be2c5206a4140b6960f6960f6826
Clare-Boniface <clarelgarfield@hotmail.com>
2023-09-02 13:49:31 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import data from "./data.js";
+
+export default function App() {
+  const attractions = data.map(function (attraction) {
+    return <Card key={attraction.id} attraction={attraction} />;
+  });
+  return (
+    <div>
+      <Header />
+      <div className="container">{attractions}</div>
+      <Footer />
+    </div>
+  );
+}
---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Footer() {
+    return (
+        <footer>
+        <p className="footer-text">&copy; My Travel Journal by Clare Boniface (September 2023)</p>
+        </footer>
+    )
+}
